5b56fc0e156a2921515fa525f1ca7a050aeb1c8e
Foxssj <nicanorgonzalez2003@gmail.com>
2023-06-09 13:16:21 +0100

    feat: added ejercicio 1 components

---
+++ src/com/salesianios/actividad1/ejercicio1.java
@@ -0,0 +1,27 @@
+package com.salesianios.actividad1;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ejercicio1 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        FileWriter fw = new FileWriter("src/com/salesianios/ficheros/ejercicio1.txt");
+        String frase = "";
+        final int MINIMO_CARACTERES = 30;
+
+        while (frase.length() < 30) {
+            System.out.println("Introduzca su frase");
+            frase = sc.nextLine();
+            if (frase.length() < 30) {
+                System.out.println("Faltan " + (MINIMO_CARACTERES - frase.length()) + " caracteres");
+            }
+        }
+
+        String fraseMayus = frase.toUpperCase().replace(" ", "_");
+        fw.write(fraseMayus);
+
+        fw.close();
+    }
+}
